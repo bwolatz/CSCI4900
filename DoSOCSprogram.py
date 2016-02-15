5f2fa351f6699e022b8ae1936ae013dd7cbc1ee2
Nikhit Adusumilli <nadusumilli@unomaha.edu>
2016-02-15 10:58:00 -0600

    removed unused variables

--- DoSOCSprogram.py
+++ DoSOCSprogram.py
@@ -5,14 +5,12 @@ import sys;
 
 #Declaring the variables for the program
 count = 0;
-pos = 0;
 results = [];
 
 #loop to check and get all the lines of output from nomos for all the files. 
 for arg in sys.argv:
         if(count > 0):
                 results.insert(pos,subprocess.check_output(["dosocs2","oneshot","%s"%(arg,)]));
-                pos += 1;
         count += 1;
 
 #Just printing all the output that is stored.
