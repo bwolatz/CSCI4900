505a70a8cc6dff41ca249f676f42976604544c17
bwolatz <bwolatz@unomaha.edu>
2016-03-09 12:29:28 -0600

    Update main.py
    Added maven process call which generates dependency tree and places it in a file called tree.txt

--- src/main.py
+++ src/main.py
@@ -4,14 +4,16 @@ import subprocess
 import glob
 import os
 
-currentdir = "/home/nikhit/Results/dependencies"
-
+currentdir = "/usr/share/fossology/nomos/agent/jars"
+pomdir = "/usr/share/fossology/nomos/agent/"
 mvncommand = "mvn -q dependency:copy-dependencies -DcopyPom=true -DoutputDirectory="+currentdir
+mvncommand2 = "mvn dependency:tree -Doutput=" + pomdir + "/tree.txt -DoutputType=text"
 
 subprocess.call(mvncommand, shell=True)
 
+os.chdir(pomdir)
+subprocess.call(mvncommand2, shell=True)
+
 os.chdir(currentdir)
 for jar in glob.glob('*.jar'):
-	subprocess.call('dosocs2 oneshot '+jar, shell=True)
-
-
+    subprocess.call('dosocs2 oneshot '+jar, shell=True)
