86f48cdcb0da68aec39df9bca9301688d15449b2
nadusumilli <nadusumilli@unomaha.edu>
2016-04-14 15:22:08 -0500

    Relationship adding changes made

--- src/dependency.py
+++ src/dependency.py
@@ -51,11 +51,9 @@ class Dependency:
                                         dep3JarsP.append(lastl2)
                 count = 0
                 for l2jar in dep2Jars:
-                        print l2jar + "\' is a dependent of \'" + dep2JarsP[count] + "\'"
                         count += 1
                 count = 0
                 for l3jar in dep3Jars:
-                        print "\'" + l3jar + "\' is a dependent of \'" + dep3JarsP[count] + "\'"
                         count += 1
                 if os.path.exists(self.tempdir):
                         os.chdir(self.tempdir)
@@ -71,20 +69,17 @@ class Dependency:
                         token = jar1.split(':')
                         jar1 = token[1]+"-"+token[3]+".jar"
                         output = subprocess.Popen('dosocs2 scan '+jar1, stdout=subprocess.PIPE ,stderr=subprocess.STDOUT, shell=True)
-                        print'printing thing'
                         line = output.stdout.readline().rstrip()
-                        print'line: \n'+line
                         match = re.match(r".*package_id:\s?(\d+)",line)
                         if(match):
-                                print 'Package ID: '+match.group(1)
+				print 'Generating level-1 documents'
                                 docinfo = subprocess.Popen('dosocs2 generate '+match.group(1), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-                                docid = docinfo.stdout.readline().rstrip()
-                                print docid
-                                docmatch = re.match(r".*document_id:\s?(\d+)",docid)
-                                print 'Document Id: '+docmatch.group(1)
-                                print subprocess.check_output(['dosocs2','print',docmatch.group(1)])
-                        print 'end'
-                        dep1JarI.append(match.group(1))
+                                #docid = docinfo.stdout.readline().rstrip()
+                                #print docid
+                                #docmatch = re.match(r".*document_id:\s?(\d+)",docid)
+                                #print 'Document Id: '+docmatch.group(1)
+                                #print subprocess.check_output(['dosocs2','print',docmatch.group(1)])
+                        	dep1JarI.append(match.group(1))
                         
                 print("Scanning level-2 dependencies")
 		for jar2 in dep2Jars:
@@ -92,20 +87,17 @@ class Dependency:
                         token = jar2.split(':')
                         jar2 = token[1] + "-" + token[3] + ".jar"
                         output = subprocess.Popen('dosocs2 scan '+ jar2, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-                        print 'printing thing'
                         line =  output.stdout.readline().rstrip()
-                        print line
                         match = re.match(r".*package_id:\s?(\d+)",line)
                         if(match):
-                                print'Package Id: '+match.group(1)
+				print 'Generating level-2 documents'
                                 docinfo = subprocess.Popen('dosocs2 generate '+match.group(1), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-                                docid = docinfo.stdout.readline().rstrip()
-                                print docid
-                                docmatch = re.match(r".*document_id:\s?(\d+)",docid)
-                                print 'Document Id: '+docmatch.group(1)
-                                print subprocess.check_output(['dosocs2','print',docmatch.group(1)])
-                        print 'end'
-                        dep2JarI.append(match.group(1))
+                                #docid = docinfo.stdout.readline().rstrip()
+                                #print docid
+                                #docmatch = re.match(r".*document_id:\s?(\d+)",docid)
+                                #print 'Document Id: '+docmatch.group(1)
+                                #print subprocess.check_output(['dosocs2','print',docmatch.group(1)])
+                        	dep2JarI.append(match.group(1))
                         
                 print("Scanning level-3 dependencies")
                 for jar3 in dep3Jars:
@@ -113,48 +105,49 @@ class Dependency:
                         token = jar3.split(':')
                         jar3 = token[1] + "-" + token[3] + ".jar"
                         output = subprocess.Popen('dosocs2 scan '+jar3,stdout = subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-                        print 'printing thing'
                         line = output.stdout.readline().rstrip()
-                        print line
                         match = re.match(r".*package_id:\s?(\d+)",line)
                         if(match):
-                                print'Package Id: '+match.group(1)
+				print 'Generating level-3 documents'
                                 docinfo = subprocess.Popen('dosocs2 generate '+match.group(1), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-                                docid = docinfo.stdout.readline().rstrip()
-                                print docid
-                                docmatch = re.match(r".*document_id:\s?(\d+)",docid)
-                                print 'Document Id: '+docmatch.group(1)
-                                print subprocess.check_output(['dosocs2','print',docmatch.group(1)])
-                        print 'end'
-                        dep3JarI.append(match.group(1))
+                                #docid = docinfo.stdout.readline().rstrip()
+                                #print docid
+                                #docmatch = re.match(r".*document_id:\s?(\d+)",docid)
+                                #print 'Document Id: '+docmatch.group(1)
+                                #print subprocess.check_output(['dosocs2','print',docmatch.group(1)])
+                        	dep3JarI.append(match.group(1))
                         
                 count = 0
                 for l2jar in dep2Jars:
                         otherI = 0
-                        thisI = dep2JarI[count]
+			if(count < len(dep2JarI)):
+                        	thisI = dep2Jars[count]
                         #print l2jar + "\' is a dependent of \'" + dep2JarsP[count] + "\'"
                         count2 = 0
                         for parentJar in dep1Jars:
                                 if dep2JarsP[count] == parentJar:
-                                        otherI = dep1JarI[count2]
+                                        if(count < len(dep1JarI)):
+						otherI = dep1Jars[count2]
                                         break
                                 count2 += 1
                         count += 1
                         if otherI != 0: 
-                                print subprocess.check_output(['dosocs2','relation',otherI,thisI])
+                        	subprocess.call(['dosocs2','relation',otherI,thisI])
                         
                         
                 count = 0
                 for l3jar in dep3Jars:
                         otherI = 0
-                        thisI = dep3JarI[count]
+                        if(count < len(dep3JarI)):
+				thisI = dep3Jars[count]
                         #print l2jar + "\' is a dependent of \'" + dep2JarsP[count] + "\'"
                         count2 = 0
                         for parentJar in dep2Jars:
                                 if dep3JarsP[count] == parentJar:
-                                        otherI = dep2JarI[count2]
+                                        if(count < len(dep2JarI)):
+						otherI = dep2Jars[count2]
                                         break
                                 count2 += 1
                         count += 1
                         if otherI != 0:
-                                print subprocess.check_output(['dosocs2','relation',otherI,thisI])
+                        	subprocess.call(['dosocs2','relation',otherI,thisI])
